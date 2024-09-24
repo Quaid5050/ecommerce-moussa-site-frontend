0db6bad54978ce75257230fc74da9c5cb5641ec6
quaid5050 <inoxent5050@gmail.com>
2024-09-24 23:00:34 +0500

    init

---
+++ src/utils/AppConfig.ts
@@ -0,0 +1,9 @@
+// FIXME: Update this configuration file based on your project information
+
+export const AppConfig = {
+  site_name: 'Quaid Ahmed',
+  title: 'Quaid Ahmed - Full Stack Developer',
+  description:
+    'Hotkicks Ecommerce Template in NextJS, React, HTML and TailwindCSS',
+  locale: 'en',
+};
